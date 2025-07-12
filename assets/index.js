71c49105928f514fed3b44a5629885f3db92ffa3
Artem8711 <goglil2063@gmail.com>
2025-07-12 12:17:14 +0200

    Добавить основной скрипт и исправить отступы в HTML

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+let sideCount = 1;
+const SIDE_AMOUNT = 4;
+
+while (sideCount <= SIDE_AMOUNT) {
+   console.log(`Пройти прямо`);
+   console.log(`Повернути наліво`);
+   //sideCount = sideCount + 1;
+   // sideCount += 1;
+   sideCount++;
+}
