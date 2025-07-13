de400880964b3e4babfd024fa6b30a8fff3181d4
Artem8711 <goglil2063@gmail.com>
2025-07-13 08:26:06 +0200

    Добавить цикл для суммирования чисел от 1 до 20 и вывод результата

--- assets/index.js
+++ assets/index.js
@@ -8,3 +8,21 @@ while (sideCount <= SIDE_AMOUNT) {
    // sideCount += 1;
    sideCount++;
 }
+
+/* Робота з циклами */
+
+let i = 1;
+let sum = 0;
+
+while (i <= 20) {
+   sum = sum + i;
+   i++;
+   console.log(i);
+}
+console.log(sum);
+
+/* краткий роспис */
+
+// i = 1;
+// i <= 20 (true);
+// (i++) = i + 1 = 1 + 1 = 2 и тд до 20;
