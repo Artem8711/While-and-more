b70dbe527e301f6751313379c18439fa226c1f90
Artem8711 <goglil2063@gmail.com>
2025-07-13 09:02:56 +0200

    Закомментировать примеры использования циклов и добавить новый цикл с шагом -0.5

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
-let sideCount = 1;
+/* let sideCount = 1;
 const SIDE_AMOUNT = 4;
 
 while (sideCount <= SIDE_AMOUNT) {
@@ -7,11 +7,11 @@ while (sideCount <= SIDE_AMOUNT) {
    //sideCount = sideCount + 1;
    // sideCount += 1;
    sideCount++;
-}
+} */
 
 /* Робота з циклами */
 
-let i = 1;
+/* let i = 1;
 let sum = 0;
 
 while (i <= 20) {
@@ -19,10 +19,30 @@ while (i <= 20) {
    i++;
    console.log(i);
 }
-console.log(sum);
+console.log(sum); */
 
 /* краткий роспис */
 
 // i = 1;
 // i <= 20 (true);
 // (i++) = i + 1 = 1 + 1 = 2 и тд до 20;
+
+//Цикл for ------------------------------------
+
+/* const TOTAL_PAGES_AMOUNT = 10; */
+
+/* for (let currentPage = 1; currentPage <= TOTAL_PAGES_AMOUNT; currentPage++) {
+   console.log('currentPage', currentPage);
+} */
+
+/* for (let i = 10; i <= 40; i = i + 2) {
+   console.log(i);
+} */
+
+/* for (let i = 10; i <= 100; i = i + 10) {
+   console.log(i);
+} */
+
+for (let i = 5; i >= -5; i = i - 0.5) {
+   console.log(i);
+}
